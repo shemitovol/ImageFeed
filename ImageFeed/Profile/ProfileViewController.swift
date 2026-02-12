3e381650b46b660f4d4537f4e7fd8e40374dda0f
shemitovol <olegshemitov@bk.ru>
2026-02-12 12:01:11 +0500

    Создан и первично настроен UITabBarController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    @IBOutlet private var profileImage: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var tagLabel: UILabel!
+    @IBOutlet private var statusLabel: UILabel!
+    @IBOutlet private var exitButton: UIButton!
+    @IBAction private func exitButtonTouchUpInside(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
+
